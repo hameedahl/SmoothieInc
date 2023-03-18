bdfe4bece3f9f3fef95849d5b452ab424f6afb37
hameedahl <hameedah_l@yahoo.com>
2023-03-18 11:24:13 -0400

    update

--- SmoothieInc/Assets/SmoothieAssets/Scripts/BlenderSlot.cs
+++ SmoothieInc/Assets/SmoothieAssets/Scripts/BlenderSlot.cs
@@ -6,12 +6,27 @@ using UnityEngine.EventSystems;
 public class BlenderSlot : MonoBehaviour, IDropHandler
 {
     private RectTransform rectTransform;
+    private Slots inventory;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Blender").GetComponent<Slots>();
+    }
 
     public void OnDrop(PointerEventData eventData) {
         /* get dragged item and place in slot */
         if (eventData.pointerDrag != null) {
-            rectTransform = eventData.pointerDrag.GetComponent<RectTransform>();
-            rectTransform.anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+            for (int i = 0; i < inventory.slots.Length; i++){
+                if (inventory.isFull[i] == false) {
+                    /* item can be added to inventory */
+                    rectTransform = eventData.pointerDrag.GetComponent<RectTransform>();
+                    rectTransform.anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+                    inventory.isFull[i] = true;
+            Debug.Log("IN");
+
+                    break;
+                }
+            }
         }
     }
 }
