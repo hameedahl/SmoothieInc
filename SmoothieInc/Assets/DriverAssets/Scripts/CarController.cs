4f07c6075cf0752b43e93d890191763856ec4e24
rorymy3 <52330390+rorymy3@users.noreply.github.com>
2023-03-25 15:37:19 -0500

    Truck, Map and Driving functionality

---
+++ SmoothieInc/Assets/DriverAssets/Scripts/CarController.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    [Header("Car Settings")]
+    public float driftFactor = 0.95f;
+    public float accelerationFactor = 30.0f;
+    public float turnFactor = 3.5f;
+    public float maxSpeed = 20;
+
+    float accelerationInput = 0;
+    float steeringInput = 0;
+
+    float rotationAngle = 0;
+
+    float velocityVsUp = 0;
+
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        ApplyEngineForce();
+
+        KillOrthogonalVelocity();
+
+        ApplySteering();
+    }
+
+    void ApplyEngineForce()
+    {
+        velocityVsUp = Vector2.Dot(transform.up, rb.velocity);
+
+        if(velocityVsUp > maxSpeed && accelerationInput > 0)
+            return;
+
+        if(velocityVsUp < -maxSpeed * 0.5f && accelerationInput < 0)
+            return;
+
+        if(rb.velocity.sqrMagnitude > maxSpeed * maxSpeed && accelerationInput > 0)
+            return;
+
+        if(accelerationInput == 0)
+            rb.drag = Mathf.Lerp(rb.drag, 3.0f, Time.fixedDeltaTime * 3);
+        else
+            rb.drag = 0;
+
+        Vector2 efVector = transform.up * accelerationInput * accelerationFactor; 
+
+        rb.AddForce(efVector, ForceMode2D.Force);
+    }
+
+    void ApplySteering()
+    {
+        float minTurnSpeed = (rb.velocity.magnitude / 8);
+        minTurnSpeed = Mathf.Clamp01(minTurnSpeed);
+
+        rotationAngle -= steeringInput * turnFactor * minTurnSpeed;
+
+        rb.MoveRotation(rotationAngle);
+    }
+
+    void KillOrthogonalVelocity()
+    {
+        Vector2 forwardVelocity = transform.up * Vector2.Dot(rb.velocity, transform.up);
+        Vector2 rightVelocity = transform.right * Vector2.Dot(rb.velocity, transform.right);
+
+        rb.velocity = forwardVelocity + rightVelocity * driftFactor;
+    }
+
+    public void SetInputVector(Vector2 inputVector)
+    {
+        steeringInput = inputVector.x;
+        accelerationInput = inputVector.y;
+    }
+}
