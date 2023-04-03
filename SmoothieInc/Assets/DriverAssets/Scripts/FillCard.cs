5fa64c44fc651e33bfa3179d28bdfa7eaad72a7c
rorymy3 <52330390+rorymy3@users.noreply.github.com>
2023-04-02 21:19:13 -0400

    Order Card setup

---
+++ SmoothieInc/Assets/DriverAssets/Scripts/FillCard.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FillCard : MonoBehaviour
+{
+    [Header("Images")]
+    public Sprite[] solids;
+    public Sprite[] liquids;
+    public Sprite[] blendTimes;
+    public Sprite[] cupSizes;
+    public Sprite[] mixIns;
+    public Sprite[] toppings;
+
+    [Header("Slots")]
+    public Image solid1;
+    public Image solid2;
+    public Image solid3;
+    public Image solid4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
