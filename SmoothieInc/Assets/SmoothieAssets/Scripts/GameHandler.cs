6fc49d4b0927eac017fc155f1dc97585e1105d68
christopher pellegrini <106196537+mrtoaf@users.noreply.github.com>
2023-04-07 19:42:35 -0400

    Updated Gamehandler.cs
    Converted the key value array to a regular array after order generatio

--- SmoothieInc/Assets/SmoothieAssets/Scripts/GameHandler.cs
+++ SmoothieInc/Assets/SmoothieAssets/Scripts/GameHandler.cs
@@ -16,13 +16,13 @@ public class GameHandler : MonoBehaviour
     const int solidsIndex = 4; /* end index */
 
     const int liquidsRange = 1; // milk
-    const int liquidsIndex = 7; 
+    const int liquidsIndex = 7;
 
     const int timeRange = 4;
     const int timeIndex = 7;
 
     const int cupsRange = 3; // S M L
-    const int cupsIndex = 8; 
+    const int cupsIndex = 8;
 
     const int mixInRange = 4;
     const int mixInIndex = 9;
@@ -34,6 +34,7 @@ public class GameHandler : MonoBehaviour
     const int emptySlot = -1;
 
     public KeyValuePair<string, int>[] order = new KeyValuePair<string, int> [arraySize];
+    int[] valuesArray = new int[arraySize];
     private GameObject cup;
     public int playerScore = 100;
     public bool orderComplete = false;
@@ -43,7 +44,7 @@ public class GameHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start() {
         for (int i = 0; i < arraySize; i++) {
-            order[i] = new KeyValuePair<string, int>("", emptySlot); 
+            order[i] = new KeyValuePair<string, int>("", emptySlot);
         }
 
         generateOrder(1);
@@ -57,6 +58,17 @@ public class GameHandler : MonoBehaviour
                 Debug.Log(kvp.Key + kvp.Value);
            // }
 
+        }
+
+        for (int i = 0; i < arraySize; i++)
+        {
+            valuesArray[i] = order[i].Value;
+        }
+
+        // Print the valuesArray for debugging purposes
+        foreach (int value in valuesArray)
+        {
+            Debug.Log(value);
         }
     }
 
@@ -77,30 +89,31 @@ public class GameHandler : MonoBehaviour
         }
     }
 
-    public void generateOrder(int difficulty) { 
+    public void generateOrder(int difficulty) {
         if (difficulty == 1) {
             for (int i = 0; i < solidsIndex; i++) {
                 order[i] = new KeyValuePair<string, int>("Solids", Random.Range(0, solidsRange));
             }
 
             for (int i = solidsIndex; i < liquidsIndex; i++) {
-                order[i] = new KeyValuePair<string, int>("Liquids", Random.Range(0, liquidsRange)); 
+                order[i] = new KeyValuePair<string, int>("Liquids", Random.Range(0, liquidsRange));
             }
 
-            // order[timeIndex] = new KeyValuePair<string, int>("Time", Random.Range(0, timeRange)); 
+            // order[timeIndex] = new KeyValuePair<string, int>("Time", Random.Range(0, timeRange));
 
-            // order[cupsIndex] = new KeyValuePair<string, int>("Cups", Random.Range(0, cupsRange)); 
+            // order[cupsIndex] = new KeyValuePair<string, int>("Cups", Random.Range(0, cupsRange));
 
-            // order[mixInIndex] = new KeyValuePair<string, int>("MixIn", Random.Range(0, mixInIndex)); 
+            // order[mixInIndex] = new KeyValuePair<string, int>("MixIn", Random.Range(0, mixInIndex));
 
 
             // for (int i = mixInIndex + 1; i < toppingsIndex + 1; i++) {
-            //     order[i] = new KeyValuePair<string, int>("Toppings", Random.Range(0, toppingsRange)); 
+            //     order[i] = new KeyValuePair<string, int>("Toppings", Random.Range(0, toppingsRange));
             // }
         // } else if (difficulty == 2) {
 
         // } else (difficulty == 3) {
 
-        } 
+        }
+
     }
 }
