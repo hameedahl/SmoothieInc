1b52c0b9d1a0031065ab279577cda1c075f0a0f4
christopher pellegrini <106196537+mrtoaf@users.noreply.github.com>
2023-04-11 15:03:37 -0400

    Added smoothie variables to the BooleanNetworkHandler

--- SmoothieInc/Assets/BooleanNetworkHandler.cs
+++ SmoothieInc/Assets/BooleanNetworkHandler.cs
@@ -6,29 +6,56 @@ using Unity.Netcode;
 public class NetworkArrivedStatus : NetworkBehaviour
 {
     public GameObject truckManagerObject;
+    public GameObject gameHandlerObject;
 
     [SerializeField]
     [NetworkVariable]
     private NetworkVariableBool arrivedNetworkVariable = new NetworkVariableBool();
 
+    [SerializeField]
+   [NetworkVariable]
+   private NetworkVariableBool drinkFinishedNetworkVariable = new NetworkVariableBool();
+
+   [SerializeField]
+   [NetworkVariable]
+   private NetworkVariableDouble playerScoreNetworkVariable = new NetworkVariableDouble();
+
     private TruckManager truckManager;
+    private GameHandler gameHandler;
 
     private void Start()
     {
         truckManager = truckManagerObject.GetComponent<TruckManager>();
+        gameHandler = gameHandlerObject.GetComponent<GameHandler>();
     }
 
     private void Update()
     {
-        if (IsServer)
-        {
-            bool isTruckArrived = truckManager.GetArrivedStatus();
-            arrivedNetworkVariable.Value = isTruckArrived;
-        }
+      if (IsHost)
+      {
+          bool isTruckArrived = truckManager.GetArrivedStatus();
+          arrivedNetworkVariable.Value = isTruckArrived;
+
+          bool isDrinkFinished = gameHandler.GetDrinkFinished();
+          drinkFinishedNetworkVariable.Value = isDrinkFinished;
+
+          double currentPlayerScore = gameHandler.GetPlayerScore();
+          playerScoreNetworkVariable.Value = currentPlayerScore;
+      }
     }
 
     public bool GetArrivedStatus()
     {
         return arrivedNetworkVariable.Value;
+    }
+
+    public bool GetDrinkFinishedStatus()
+    {
+        return drinkFinishedNetworkVariable.Value;
+    }
+
+    public double GetPlayerScoreStatus()
+    {
+        return playerScoreNetworkVariable.Value;
     }
 }
