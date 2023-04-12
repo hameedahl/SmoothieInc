a9e75b3d8a21a60f70374df381021da8be7351a6
christopher pellegrini <106196537+mrtoaf@users.noreply.github.com>
2023-04-12 15:19:24 -0400

    Added relay stuff

--- SmoothieInc/Assets/NetworkUIManager.cs
+++ SmoothieInc/Assets/NetworkUIManager.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Unity.Netcode;
+using TMPro;
 
 public class NetworkUIManager : MonoBehaviour
 {
 
   public Button hostButton;
   public Button clientButton;
+  public TMP_InputField joinCodeInput;
 
   public Camera truckCamera;
   public Camera smoothieCamera;
@@ -16,104 +18,38 @@ public class NetworkUIManager : MonoBehaviour
   public GameObject truckUI;
   public GameObject smoothieUI;
 
+
+  public TestRelay testRelay;
+
+
+
   private void Awake()
   {
 
      hostButton.onClick.AddListener(() => {NetworkManager.Singleton.StartHost();
                                            truckCamera.gameObject.SetActive(true);
                                            truckUI.gameObject.SetActive(true);
+                                           testRelay.CreateRelay();
                                             });
+
      clientButton.onClick.AddListener(() => {NetworkManager.Singleton.StartClient();
                                              smoothieCamera.gameObject.SetActive(true);
                                              smoothieUI.gameObject.SetActive(true);
-
+                                             testRelay.JoinRelay(joinCodeInput.text);
                                             });
 
 
   }
 
+  private void FixedUpdate()
+  {
+    if(joinCodeInput.text.Length >= 6)
+    {
+        joinCodeInput.text = joinCodeInput.text.Substring(0,6);
+        clientButton.interactable = true;
+    }
+    else
+      clientButton.interactable = false;
+  }
+
 }
-
-/*
-using System.Collections;
-using UnityEngine;
-using UnityEngine.UI;
-using Unity.Netcode;
-using Unity.Services.Relay;
-
-public class NetworkManagerCustom : MonoBehaviour
-{
-    public Button hostButton;
-    public Button clientButton;
-
-    public Camera truckCamera;
-    public Camera smoothieCamera;
-
-    public GameObject truckUI;
-    public GameObject smoothieUI;
-
-    [SerializeField] private InputField joinCodeInputField;
-    [SerializeField] private Text joinCodeText;
-
-    private void Start()
-    {
-        hostButton.onClick.AddListener(() =>
-        {
-            StartHost();
-            truckCamera.gameObject.SetActive(true);
-            truckUI.gameObject.SetActive(true);
-        });
-
-        clientButton.onClick.AddListener(() =>
-        {
-            StartClient();
-            smoothieCamera.gameObject.SetActive(true);
-            smoothieUI.gameObject.SetActive(true);
-        });
-    }
-
-    public void StartHost()
-    {
-        StartCoroutine(RequestJoinCode());
-    }
-
-    private IEnumerator RequestJoinCode()
-    {
-        var request = new RelayServerAllocationRequest();
-        var handle = RelayService.Instance.CreateRelayServer(request);
-        yield return handle;
-
-        if (handle.Status == AsyncOperationStatus.Succeeded)
-        {
-            var response = handle.Result;
-            NetworkManager.Singleton.GetComponent<UnityTransport>().ServerListenPort = response.Port;
-            joinCodeText.text = $"Join Code: {response.JoinCode}";
-            NetworkManager.Singleton.StartHost();
-        }
-    }
-
-    public void StartClient()
-    {
-        string joinCode = joinCodeInputField.text;
-        if (!string.IsNullOrEmpty(joinCode))
-        {
-            StartCoroutine(ConnectWithJoinCode(joinCode));
-        }
-    }
-
-    private IEnumerator ConnectWithJoinCode(string joinCode)
-    {
-        var request = new RelayJoinCodeRequest { JoinCode = joinCode };
-        var handle = RelayService.Instance.JoinRelayServerWithJoinCode(request);
-        yield return handle;
-
-        if (handle.Status == AsyncOperationStatus.Succeeded)
-        {
-            var response = handle.Result;
-            NetworkManager.Singleton.GetComponent<UnityTransport>().ConnectAddress = response.IpAddress;
-            NetworkManager.Singleton.GetComponent<UnityTransport>().ConnectPort = response.Port;
-            NetworkManager.Singleton.StartClient();
-        }
-    }
-}
-*/
--- SmoothieInc/Assets/TestRelay.cs
+++ SmoothieInc/Assets/TestRelay.cs
@@ -1,18 +1,85 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using Unity.Services.Authentication;
+using TMPro;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
 
 public class TestRelay : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+  public TMP_Text joinCodeDisplay;
+  public TMP_InputField joinCodeInput;
+
+  private async void Start() {
+    await UnityServices.InitializeAsync();
+
+    AuthenticationService.Instance.SignedIn += () => {
+      Debug.Log("Signed in " + AuthenticationService.Instance.PlayerId);
+    };
+    await AuthenticationService.Instance.SignInAnonymouslyAsync();
+  }
+
+  public async void CreateRelay() {
+    try {
+      Allocation allocation = await RelayService.Instance.CreateAllocationAsync(2);
+
+      string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+      joinCodeDisplay.text = joinCode;
+      Debug.Log(joinCode);
+
+      RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
+      NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+      // NetworkManager.Singleton.GetComponent<UnityTransport>().SetHostRelayData(
+      //   allocation.RelayServer.IpV4,
+      //   (ushort)allocation.RelayServer.Port,
+      //   allocation.AllocationIdBytes,
+      //   allocation.Key,
+      //   allocation.ConnectionData
+      // );
+
+      NetworkManager.Singleton.StartHost();
+
+    } catch (RelayServiceException e) {
+      Debug.Log(e);
     }
+  }
+
+  public async void JoinRelay(string joinCode) {
+
+    try {
+      Debug.Log("Joining Relay with " + joinCode);
+      joinCode = joinCode.Substring(0,6);
+      JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+
+      RelayServerData relayServerData = new RelayServerData(joinAllocation, "dtls");
+      NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+
+
+      // NetworkManager.Singleton.GetComponent<UnityTransport>().SetClientRelayData(
+      //   joinAllocation.RelayServer.IpV4,
+      //   (ushort)joinAllocation.RelayServer.Port,
+      //   joinAllocation.AllocationIdBytes,
+      //   joinAllocation.Key,
+      //   joinAllocation.ConnectionData,
+      //   joinAllocation.HostConnectionData
+      // );
+
+      NetworkManager.Singleton.StartClient();
+
+    } catch (RelayServiceException e) {
+      Debug.Log(e);
+    }
+  }
+
+
+
+
+
 }
