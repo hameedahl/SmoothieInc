03bafa45e9429c671192b9209f8a93ccaa3554a0
christopher pellegrini <106196537+mrtoaf@users.noreply.github.com>
2023-04-16 15:41:48 -0400

    ADDED THE ORDER ARRAY TO THE NETWORK

--- SmoothieInc/Assets/BooleanNetworkHandler.cs
+++ SmoothieInc/Assets/BooleanNetworkHandler.cs
@@ -7,6 +7,7 @@ public class BooleanNetworkHandler : NetworkBehaviour
 {
     public GameObject truckManagerObject;
     public GameObject gameHandlerObject;
+    public FillCard fillCard;
 
     bool started = false;
 
@@ -32,14 +33,121 @@ public class BooleanNetworkHandler : NetworkBehaviour
       NetworkVariableWritePermission.Owner
     );
 
+    private NetworkVariable<int> valuesArrayNetwork0 = new NetworkVariable<int>(
+      value:0,
+      NetworkVariableReadPermission.Everyone,
+      NetworkVariableWritePermission.Owner
+    );
+
+    private NetworkVariable<int> valuesArrayNetwork1 = new NetworkVariable<int>(
+      value:0,
+      NetworkVariableReadPermission.Everyone,
+      NetworkVariableWritePermission.Owner
+    );
+
+    private NetworkVariable<int> valuesArrayNetwork2 = new NetworkVariable<int>(
+      value:0,
+      NetworkVariableReadPermission.Everyone,
+      NetworkVariableWritePermission.Owner
+    );
+
+    private NetworkVariable<int> valuesArrayNetwork3 = new NetworkVariable<int>(
+      value:0,
+      NetworkVariableReadPermission.Everyone,
+      NetworkVariableWritePermission.Owner
+    );
+
+    private NetworkVariable<int> valuesArrayNetwork4 = new NetworkVariable<int>(
+      value:0,
+      NetworkVariableReadPermission.Everyone,
+      NetworkVariableWritePermission.Owner
+    );
+
+    private NetworkVariable<int> valuesArrayNetwork5 = new NetworkVariable<int>(
+      value:0,
+      NetworkVariableReadPermission.Everyone,
+      NetworkVariableWritePermission.Owner
+    );
+
+    private NetworkVariable<int> valuesArrayNetwork6 = new NetworkVariable<int>(
+      value:0,
+      NetworkVariableReadPermission.Everyone,
+      NetworkVariableWritePermission.Owner
+    );
+
+    private NetworkVariable<int> valuesArrayNetwork7 = new NetworkVariable<int>(
+      value:0,
+      NetworkVariableReadPermission.Everyone,
+      NetworkVariableWritePermission.Owner
+    );
+
+    private NetworkVariable<int> valuesArrayNetwork8 = new NetworkVariable<int>(
+      value:0,
+      NetworkVariableReadPermission.Everyone,
+      NetworkVariableWritePermission.Owner
+    );
+
+    private NetworkVariable<int> valuesArrayNetwork9 = new NetworkVariable<int>(
+      value:0,
+      NetworkVariableReadPermission.Everyone,
+      NetworkVariableWritePermission.Owner
+    );
+
+    private NetworkVariable<int> valuesArrayNetwork10 = new NetworkVariable<int>(
+      value:0,
+      NetworkVariableReadPermission.Everyone,
+      NetworkVariableWritePermission.Owner
+    );
+
+    private NetworkVariable<int> valuesArrayNetwork11 = new NetworkVariable<int>(
+      value:0,
+      NetworkVariableReadPermission.Everyone,
+      NetworkVariableWritePermission.Owner
+    );
+
     private TruckManager truckManager;
     private GameHandler gameHandler;
     public Transform target;
+
+    public int[] tempValuesArray = new int[12];
+    public int[] valuesArray;
 
     private void Start()
     {
         truckManager = truckManagerObject.GetComponent<TruckManager>();
         gameHandler = gameHandlerObject.GetComponent<GameHandler>();
+
+        if (IsHost) {
+
+          valuesArray = gameHandler.GetValuesArray();
+          valuesArrayNetwork0.Value = valuesArray[0];
+          valuesArrayNetwork1.Value = valuesArray[1];
+          valuesArrayNetwork2.Value = valuesArray[2];
+          valuesArrayNetwork3.Value = valuesArray[3];
+          valuesArrayNetwork4.Value = valuesArray[4];
+          valuesArrayNetwork5.Value = valuesArray[5];
+          valuesArrayNetwork6.Value = valuesArray[6];
+          valuesArrayNetwork7.Value = valuesArray[7];
+          valuesArrayNetwork8.Value = valuesArray[8];
+          valuesArrayNetwork9.Value = valuesArray[9];
+          valuesArrayNetwork10.Value = valuesArray[10];
+          valuesArrayNetwork11.Value = valuesArray[11];
+
+
+            tempValuesArray[0] = valuesArrayNetwork0.Value;
+            tempValuesArray[1] = valuesArrayNetwork1.Value;
+            tempValuesArray[2] = valuesArrayNetwork2.Value;
+            tempValuesArray[3] = valuesArrayNetwork3.Value;
+            tempValuesArray[4] = valuesArrayNetwork4.Value;
+            tempValuesArray[5] = valuesArrayNetwork5.Value;
+            tempValuesArray[6] = valuesArrayNetwork6.Value;
+            tempValuesArray[7] = valuesArrayNetwork7.Value;
+            tempValuesArray[8] = valuesArrayNetwork8.Value;
+            tempValuesArray[9] = valuesArrayNetwork9.Value;
+            tempValuesArray[10] = valuesArrayNetwork10.Value;
+            tempValuesArray[11] = valuesArrayNetwork11.Value;
+            fillCard.Initialize(tempValuesArray);
+        }
     }
 
     private void Update()
@@ -56,11 +164,42 @@ public class BooleanNetworkHandler : NetworkBehaviour
           bool isTruckArrived = truckManager.GetArrivedStatus();
           arrivedNetworkVariable.Value = isTruckArrived;
 
+
           // bool isDrinkFinished = gameHandler.GetDrinkFinished();
           // drinkFinishedNetworkVariable.Value = isDrinkFinished;
 
           // double currentPlayerScore = gameHandler.GetPlayerScore();
           // playerScoreNetworkVariable.Value = currentPlayerScore;
+
+
+          valuesArray = gameHandler.GetValuesArray();
+          valuesArrayNetwork0.Value = valuesArray[0];
+          valuesArrayNetwork1.Value = valuesArray[1];
+          valuesArrayNetwork2.Value = valuesArray[2];
+          valuesArrayNetwork3.Value = valuesArray[3];
+          valuesArrayNetwork4.Value = valuesArray[4];
+          valuesArrayNetwork5.Value = valuesArray[5];
+          valuesArrayNetwork6.Value = valuesArray[6];
+          valuesArrayNetwork7.Value = valuesArray[7];
+          valuesArrayNetwork8.Value = valuesArray[8];
+          valuesArrayNetwork9.Value = valuesArray[9];
+          valuesArrayNetwork10.Value = valuesArray[10];
+          valuesArrayNetwork11.Value = valuesArray[11];
+
+          tempValuesArray[0] = valuesArrayNetwork0.Value;
+          tempValuesArray[1] = valuesArrayNetwork1.Value;
+          tempValuesArray[2] = valuesArrayNetwork2.Value;
+          tempValuesArray[3] = valuesArrayNetwork3.Value;
+          tempValuesArray[4] = valuesArrayNetwork4.Value;
+          tempValuesArray[5] = valuesArrayNetwork5.Value;
+          tempValuesArray[6] = valuesArrayNetwork6.Value;
+          tempValuesArray[7] = valuesArrayNetwork7.Value;
+          tempValuesArray[8] = valuesArrayNetwork8.Value;
+          tempValuesArray[9] = valuesArrayNetwork9.Value;
+          tempValuesArray[10] = valuesArrayNetwork10.Value;
+          tempValuesArray[11] = valuesArrayNetwork11.Value;
+
+          fillCard.Initialize(tempValuesArray);
 
       }
       else
@@ -83,6 +222,12 @@ public class BooleanNetworkHandler : NetworkBehaviour
     public double GetPlayerScoreStatus()
     {
         return playerScoreNetworkVariable.Value;
+    }
+
+    public int[] GetValuesArrayFromNetwork()
+    {
+
+      return tempValuesArray;
     }
 
     [ServerRpc(RequireOwnership = false)]
